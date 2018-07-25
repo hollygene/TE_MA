4f31ea4a1aed78d92f2ef805ee0454c6f051cb1f
hollygene <hmcqueary@uga.edu>
2018-07-25 11:30:10 -0400

    converting aligned reads to sam and then bam files for use in mcclintock

--- TE_MA_pipeline.sh
+++ TE_MA_pipeline.sh
@@ -11,29 +11,29 @@
 
 cd /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA
 
-module load java/jdk1.8.0_20 fastqc
-time fastqc -o QCed_ancestors_spike_ins HM_H0_S16_R1_001.fastq.gz HM_D20_S15_R1_001.fastq.gz HMM_D1_S14_R1_001.fastq.gz HMM_D0_S13_R1_001.fastq.gz
+#module load java/jdk1.8.0_20 fastqc
+#time fastqc -o QCed_ancestors_spike_ins HM_H0_S16_R1_001.fastq.gz HM_D20_S15_R1_001.fastq.gz HMM_D1_S14_R1_001.fastq.gz HMM_D0_S13_R1_001.fastq.gz
 
 #######################################################################################
 
 cd /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA
 
-module load fastx/0.0.14
-time fastx_trimmer -f 10 \
--i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HM_H0_S16_R1_001.fastq \
- -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0_trim.fastq
+#module load fastx/0.0.14
+#time fastx_trimmer -f 10 \
+#-i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HM_H0_S16_R1_001.fastq \
+# -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0_trim.fastq
 
- time fastx_trimmer -f 10 \
- -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HM_D20_S15_R1_001.fastq \
-  -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20_trim.fastq
+# time fastx_trimmer -f 10 \
+# -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HM_D20_S15_R1_001.fastq \
+#  -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20_trim.fastq
 
-  time fastx_trimmer -f 10 \
-  -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HMM_D1_S14_R1_001.fastq \
-   -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D1_trim.fastq
+#  time fastx_trimmer -f 10 \
+#  -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HMM_D1_S14_R1_001.fastq \
+#   -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D1_trim.fastq
 
-   time fastx_trimmer -f 10 \
-   -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HMM_D0_S13_R1_001.fastq \
-    -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D0_trim.fastq
+#   time fastx_trimmer -f 10 \
+#   -i /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/HMM_D0_S13_R1_001.fastq \
+#    -o /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D0_trim.fastq
 
  #######################################################################################
 
@@ -41,20 +41,60 @@ time fastx_trimmer -f 10 \
  module load bwa/0.7.15
 
  #index the ref genome
- bwa index /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna
+ #bwa index /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna
 
- time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
-/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0_trim.fastq \
- > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_H0_trim.output.fastq.align.sai
+ #time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0_trim.fastq \
+ #> /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_H0_trim.output.fastq.align.sai
 
- time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
-/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20_trim.fastq \
- > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_D20_trim.output.fastq.align.sai
+ #time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20_trim.fastq \
+ #> /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_D20_trim.output.fastq.align.sai
 
- time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
-/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D1_trim.fastq \
- > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D1_trim.output.fastq.align.sai
+ #time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D1_trim.fastq \
+# > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D1_trim.output.fastq.align.sai
 
- time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
-/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D0_trim.fastq \
- > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D0_trim.output.fastq.align.sai
+ #time bwa aln /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#/lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D0_trim.fastq \
+# > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D0_trim.output.fastq.align.sai
+
+ ###########################################################################################
+
+ #create the sam files
+
+#bwa samse /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#     /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_H0_trim.output.fastq.align.sai \
+#     /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0_trim.fastq \
+#     > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0.sam
+
+#bwa samse /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#          /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D0_trim.output.fastq.align.sai \
+#          /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D0_trim.fastq \
+#          > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D0.sam
+
+#bwa samse /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#                    /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HMM_D1_trim.output.fastq.align.sai \
+#                    /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HMM_D1_trim.fastq \
+#                    > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D1.sam
+
+#bwa samse /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna \
+#        /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/align/HM_D20_trim.output.fastq.align.sai \
+#        /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20_trim.fastq \
+#        > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20.sam
+
+#########################################################################################
+
+#samtools
+module load samtools/1.6
+
+#index reference genome
+#samtools faidx /lustre1/hcm14449/TE_MA_Paradoxus/ref_genome/paradoxus_gen.fna
+
+#samtools view -bt paradoxus_gen.fna.fai /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0.sam  > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_H0.bam
+
+#samtools view -bt paradoxus_gen.fna.fai /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D0.sam  > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D0.bam
+
+#samtools view -bt paradoxus_gen.fna.fai /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D1.sam  > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D1.bam
+
+#samtools view -bt paradoxus_gen.fna.fai /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20.sam  > /lustre1/hcm14449/TE_MA_Paradoxus/Holly_gDNA/trim/HM_D20.bam 
