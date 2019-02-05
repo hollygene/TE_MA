d6f6892fd964725ca278579f6a3cf5603fb9e5e1
hollygene <hmcqueary@uga.edu>
2019-02-05 11:56:09 -0500

    trying assigning a variable name

--- muver.sh
+++ muver.sh
@@ -9,11 +9,12 @@ cd $PBS_O_WORKDIR
 
 data_dir="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/samples"
 
+ref_genome="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa"
 
 module load muver/0.1.0-foss-2016b-Python-2.7.14
 
 #index reference genome
-muver index_reference /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa
+muver index-reference ${ref_genome}
 
 # create repeat file for reference genome
-muver create_repeat_file /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa > /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome_Repeats.txt
+# muver create-repeat-file /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa > /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome_Repeats.txt
