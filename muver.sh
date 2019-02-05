d8a9a6e589c81f7067a4b03c7021deda4ded5d41
hollygene <hmcqueary@uga.edu>
2019-02-05 12:06:32 -0500

    muver

--- muver.sh
+++ muver.sh
@@ -12,9 +12,11 @@ data_dir="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/samples"
 ref_genome="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa"
 
 module load muver/0.1.0-foss-2016b-Python-2.7.14
+# java -jar $EBROOTGATK/GenomeAnalysisTK.jar
 
 #index reference genome
-muver index-reference ${ref_genome}
+# echo ${ref_genome}
+muver index-reference /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa
 
 # create repeat file for reference genome
 # muver create-repeat-file /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa > /scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome_Repeats.txt
