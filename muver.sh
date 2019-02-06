41cf472313774da6ccce078be0b0cf60b2683fb7
hollygene <hmcqueary@uga.edu>
2019-02-06 11:28:25 -0500

    trying one more time before asking for help again

--- muver.sh
+++ muver.sh
@@ -9,7 +9,7 @@ module load muver/0.1.0-foss-2016b-Python-2.7.14
 cd $PBS_O_WORKDIR
 # mkdir "/scratch/hcm14449/TE_MA_Paradoxus/Practice/output"
 data_dir="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/samples"
-ref_genome="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fai"
+ref_genome="/scratch/hcm14449/TE_MA_Paradoxus/Practice/files/ref_genome/SCerevisiae.RefGenome.fa"
 fastq_list="/home/hcm14449/Github/TE_MA/FASTQ_LIST.txt"
 control_sample_name="Ancestor"
 experiment_directory="/scratch/hcm14449/TE_MA_Paradoxus/Practice/output"
