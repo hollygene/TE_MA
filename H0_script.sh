5848f6c9fe7a4dbfd665e350e0455efeda7bb444
Holly <hmcqueary@uga.edu>
2019-11-19 12:26:29 -0500

    invalid option -o

--- H0_script.sh
+++ H0_script.sh
@@ -79,17 +79,17 @@ module load ${samtools_module}
 # ### sort the bam files
 # ############################
 
-for file in ${output_directory}/H0/*.bam
-
-do
-
-FBASE=$(basename $file .bam)
-BASE=${FBASE%.bam}
-
-samtools sort -@ 12 -o ${output_directory}/H0/${BASE}.sorted.bam \
-   ${output_directory}/H0/${BASE}.bam
-
-done
+# for file in ${output_directory}/H0/*.bam
+#
+# do
+#
+# FBASE=$(basename $file .bam)
+# BASE=${FBASE%.bam}
+#
+# samtools sort -@ 12 -o ${output_directory}/H0/${BASE}.sorted.bam \
+#    ${output_directory}/H0/${BASE}.bam
+#
+# done
 
 # ############################
 # ### index the bam files
@@ -102,7 +102,7 @@ do
 FBASE=$(basename $file .sorted.bam)
 BASE=${FBASE%.sorted.bam}
 
-samtools index -@ 12 -o ${output_directory}/H0/${BASE}.sorted.bam
+samtools index -@ 12 ${output_directory}/H0/${BASE}.sorted.bam
 
 done
 # ###################################################################################################
