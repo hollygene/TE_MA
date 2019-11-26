de9a72ef82919f699690322bee79999367a4550e
Holly <hmcqueary@uga.edu>
2019-11-26 12:25:14 -0500

    spaces

--- D0_scripts.sh
+++ D0_scripts.sh
@@ -158,34 +158,20 @@ BASE=${FBASE%_markilluminaadapters.bam}
 
 java -Xmx20g -classpath "/usr/local/apps/eb/picard/2.4.1-Java-1.8.0_144" -jar  \
 /usr/local/apps/eb/picard/2.4.1-Java-1.8.0_144/picard.jar SamToFastq \
-
 I=${raw_data}/${BASE}_markilluminaadapters.bam \
-
 FASTQ=STDOUT \
-
 CLIPPING_ATTRIBUTE=XT CLIPPING_ACTION=2 INTERLEAVE=true NON_PF=true \
-
 TMP_DIR=${raw_data}/TMP | \
-
 bwa mem -M -t 7 -p ${ref_genome} STDIN | \
-
 java -Xmx20g -classpath "/usr/local/apps/eb/picard/2.4.1-Java-1.8.0_144" -jar  \
 /usr/local/apps/eb/picard/2.4.1-Java-1.8.0_144/picard.jar MergeBamAlignment \
-
 ALIGNED_BAM=STDIN \
-
 UNMAPPED_BAM=${raw_data}/${BASE}_fastqtosam.bam \
-
 OUTPUT=${raw_data}/${BASE}_piped.bam \
-
 R=${ref_genome} CREATE_INDEX=true ADD_MATE_CIGAR=true \
-
 CLIP_ADAPTERS=false CLIP_OVERLAPPING_READS=true \
-
 INCLUDE_SECONDARY_ALIGNMENTS=true MAX_INSERTIONS_OR_DELETIONS=-1 \
-
 PRIMARY_ALIGNMENT_STRATEGY=MostDistant ATTRIBUTES_TO_RETAIN=XS \
-
 TMP_DIR=${raw_data}/TMP
 
 done
