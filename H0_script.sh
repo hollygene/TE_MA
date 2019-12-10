4f26a39035e3c757ad227056c50e1c0b0c09efbb
Holly <hmcqueary@uga.edu>
2019-12-10 12:20:07 -0500

    forgot to increase memory

--- H0_script.sh
+++ H0_script.sh
@@ -1,9 +1,9 @@
 #PBS -S /bin/bash
 #PBS -q batch
-#PBS -N H0_HC_48
-#PBS -l nodes=1:ppn=1:AMD
+#PBS -N H0_BQSR_HC_GT
+#PBS -l nodes=2:ppn=1:AMD
 #PBS -l walltime=120:00:00
-#PBS -l mem=100gb
+#PBS -l mem=505gb
 #PBS -M hcm14449@uga.edu
 #PBS -m abe
 
