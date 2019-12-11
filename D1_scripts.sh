48de9f32a914cba5877866cebcf5ebd635c696d0
Holly <hmcqueary@uga.edu>
2019-12-10 21:51:56 -0500

    genotype gvcfs, then filter D1-A variants, then combine variantsthen Vartotable

--- D1_scripts.sh
+++ D1_scripts.sh
@@ -445,21 +445,110 @@ module load ${GATK_module}
 # ###################################################################################################
 # ###################################################################################################
 # #
-# for file in ${output_directory}/${BASE}*_variants.Recal.g.vcf
-#
-# do
-#
-# FBASE=$(basename $file _variants.Recal.g.vcf)
-#   BASE=${FBASE%_variants.Recal.g.vcf}
-#
-# time gatk GenotypeGVCFs \
-#      -R ${ref_genome} \
-#      --variant ${output_directory}/${BASE}_variants.Recal.g.vcf \
-#      -O ${output_directory}/${BASE}.vcf
-#
-#   done
+for file in ${output_directory}/${BASE}*_variants.g.vcf
+
+do
+
+FBASE=$(basename $file _variants.g.vcf)
+  BASE=${FBASE%_variants.g.vcf}
+
+time gatk GenotypeGVCFs \
+     -R ${ref_genome} \
+     --variant ${output_directory}/${BASE}_variants.g.vcf \
+     -O ${output_directory}/${BASE}.vcf
+
+done
+
+# filter based on genotype in the Ancestor only
+time gatk GenotypeGVCFs \
+     -R ${ref_genome} \
+     --variant ${output_directory}/HM-D1-A_variants.g.vcf \
+     -O ${output_directory}/HM-D1-A.vcf
 
 
+gatk VariantFiltration \
+-V ${output_directory}/HM-D1-A.vcf \
+-O ${output_directory}/HM-D1-A_hetsFil.vcf \
+--genotype-filter-expression "isHet == 1" \
+--genotype-filter-name "isHetFilter"
+
+
+gatk SelectVariants \
+-V ${output_directory}/HM-D1-A_hetsFil.vcf \
+--set-filtered-gt-to-nocall \
+-O ${output_directory}/HM-D1-A_hetsFil_SV.vcf
+
+
+time gatk SelectVariants \
+       -R ${ref_genome} \
+       -V ${output_directory}/HM-D1-A_hetsFil_SV.vcf \
+       -O ${output_directory}/HM-D1-A_hetsFil_SV_noNoCalls.vcf \
+       --max-nocall-number 0
+# ###################################################################################################
+### Combine VCFs together
+# ###################################################################################################
+module load GATK/3.8-1-Java-1.8.0_144
+
+java -jar $EBROOTGATK/GenomeAnalysisTK.jar \
+   -T CombineVariants \
+   -R ${ref_genome} \
+   -V ${output_directory}/HM-D1-A_hetsFil_SV_noNoCalls.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-10.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-20.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-31.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-42.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-21.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-32.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-44.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-11.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-22.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-33.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-45.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-12.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-24.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-35.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-46.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-13.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-25.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-36.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-4.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-14.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-26.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-37.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-5.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-15.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-27.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-38.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-6.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-16.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-28.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-39.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-7.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-17.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-29.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-3.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-8.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-18.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-2.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-9.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-19.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-30.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-40.vcf \
+      -V /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/HM-D1-41.vcf \
+      -o ${output_directory}/fullCohort.vcf \
+      -genotypeMergeOptions UNIQUIFY
+
+module unload GATK/3.8-1-Java-1.8.0_144
+
+
+
+module load ${GATK_module}
+
+gatk VariantsToTable \
+            -V ${output_directory}/fullCohort.vcf \
+            -F CHROM -F POS -F REF -F ALT -F QUAL -F DP \
+            -GF AD -GF GT -GF DP \
+            -O ${output_directory}/fullCohort.txt
 # ###################################################################################################
 ### Run GenotypeGVCFs on recalibrated samples
 # ###################################################################################################
@@ -541,7 +630,75 @@ module load ${GATK_module}
        # ###################################################################################################
        # ### joint genotype vcfs
        # ###################################################################################################
-time gatk GenotypeGVCFs \
-         -R ${ref_genome} \
-         --variant /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/cohort.g.vcf \
-         -O ${output_directory}/Full_cohort.vcf
+# time gatk GenotypeGVCFs \
+#          -R ${ref_genome} \
+#          --variant /scratch/hcm14449/TE_MA_Paradoxus/Illumina_Data/Out/D1/cohort.g.vcf \
+#          -O ${output_directory}/Full_cohort.vcf
+#
+
+
+
+# time gatk SelectVariants \
+#             -R ${ref_genome} \
+#             -V ${output_directory}/Full_cohort.vcf \
+#             -O ${output_directory}/Full_cohort_noNonVars.vcf \
+#             --exclude-non-variants true
+#
+#           time gatk SelectVariants \
+#                         -R ${ref_genome} \
+#                         -V ${output_directory}/Full_cohort_noNonVars.vcf \
+#                         -O ${output_directory}/Full_cohort_noNonVars_noFils.vcf \
+#                         --exclude-filtered true
+
+
+
+# time gatk SelectVariants \
+#        -R ${ref_genome} \
+#        -V ${output_directory}/Full_cohort.vcf \
+#        -O ${output_directory}/test.vcf \
+#        -sample-expressions '!vc.getGenotype('HM-D1-A').isHet()'
+
+       # time gatk SelectVariants \
+       #        -R ${ref_genome} \
+       #        -V ${output_directory}/Full_cohort_VF_SV.vcf \
+       #        -O ${output_directory}/Full_cohort_VF_SV_noNoCalls.vcf \
+       #        --max-nocall-number 45
+       #
+       #
+       #
+       #        gatk VariantsToTable \
+       #             -V ${output_directory}/Full_cohort_VF_SV_noNoCalls.vcf \
+       #             -F CHROM -F POS -F REF -F ALT -F QUAL -F DP \
+       #             -GF AD -GF GT -GF DP \
+       #             -O ${output_directory}/Full_cohort_VF_SV_noNoCalls.txt
+
+
+# -G_filter isHet == 1
+# -G_filterName
+# --filterName One --filterExpression "X < 1" --filterName Two --filterExpression "X > 2"
+
+
+
+# gatk VariantFiltration \
+# -V ${output_directory}/Full_cohort.vcf \
+# -O ${output_directory}/Full_cohort_VF.vcf \
+# --genotype-filter-expression "isHet == 1" \
+# --genotype-filter-name "isHetFilter"
+#
+#
+# gatk SelectVariants \
+# -V ${output_directory}/Full_cohort_VF.vcf \
+# --set-filtered-gt-to-nocall \
+# -O ${output_directory}/Full_cohort_VF_SV.vcf
+
+
+### What if I individually genotype all of my GVCFs
+# Then in the Ancestor, remove everything that is heterozygous
+# Then combine my VCFs based on POS and CHROM
+# That should give me a giant file with every site in every line that is NOT variant in the Ancestor
+      # in theory...
+# env means esclude not variant loci
+# ef means exclude filtered loci
+
+            # vc.getGenotype('sample08').isHomVar()
+            # vc.getGenotype('sample08').isHomRef()
