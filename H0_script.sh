7d5016a15060d3a4ed12cd701c6150f0e2bc3256
Holly <hmcqueary@uga.edu>
2020-03-12 16:50:59 -0400

    had diploidy in haplotype caller the second time

--- H0_script.sh
+++ H0_script.sh
@@ -1401,7 +1401,7 @@ done
         -R ${ref_genome} \
         -ERC GVCF \
         -I ${output_directory}/${BASE}_recalibratedNewRef.bam \
-        -ploidy 2 \
+        -ploidy 1 \
         -O ${output_directory}/${BASE}_variants.Recal.g.vcf
         done
 
