680df5fe4975baf36da52e49f9ce43721483e176
Holly <hmcqueary@uga.edu>
2020-04-28 12:55:04 -0400

    need to sort before indexing

--- maskFASTA.sh
+++ maskFASTA.sh
@@ -62,6 +62,7 @@ BASE=${FBASE%.bam}
 # bwa aln ${ref_genome} ${raw_data}/Anc_SpikeIns/${BASE}.fastq.gz > ${raw_data}/Anc_SpikeIns/${BASE}.sai
 # bwa samse ${ref_genome} ${raw_data}/Anc_SpikeIns/${BASE}.sai ${raw_data}/Anc_SpikeIns/${BASE}.fastq.gz > ${raw_data}/Anc_SpikeIns/${BASE}.sam
 # samtools view -b ${raw_data}/Anc_SpikeIns/${BASE}.sam > ${raw_data}/Anc_SpikeIns/${BASE}.bam
+samtools sort ${raw_data}/Anc_SpikeIns/${BASE}.bam > ${raw_data}/Anc_SpikeIns/${BASE}_sorted.bam
 samtools index ${raw_data}/Anc_SpikeIns/${BASE}.bam
 
 done
